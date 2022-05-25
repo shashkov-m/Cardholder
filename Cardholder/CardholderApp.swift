98132e56063582c074822806510f9901b0188b2c
Max <ishashkov.m@gmail.com>
2022-05-25 18:24:50 +0300

    Initial Commit

---
+++ Cardholder/CardholderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardholderApp.swift
+//  Cardholder
+//
+//  Created by Max Shashkov on 25.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CardholderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
