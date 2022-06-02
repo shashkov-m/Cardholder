90146d10a8219736c30e9a21fa369945a585ef52
Max <ishashkov.m@gmail.com>
2022-06-03 00:23:36 +0300

    View extentions
    Keyboard observer + keyboard hide func

---
+++ Cardholder/Extentions/KeyboardPublisher+View.swift
@@ -0,0 +1,26 @@
+//
+//  KeyboardPublisher+View.swift
+//  Cardholder
+//
+//  Created by Max Shashkov on 03.06.2022.
+//
+
+import SwiftUI
+import Combine
+
+extension View {
+  var keyboardPublisher: AnyPublisher<Bool, Never> {
+    Publishers
+      .Merge(
+        NotificationCenter
+          .default
+          .publisher(for: UIResponder.keyboardWillShowNotification)
+          .map { _ in true },
+        NotificationCenter
+          .default
+          .publisher(for: UIResponder.keyboardWillHideNotification)
+          .map { _ in false })
+      .debounce(for: .seconds(0.1), scheduler: RunLoop.main)
+      .eraseToAnyPublisher()
+  }
+}
