4b865d64d484463ae630526e765d51d392ddbb49
Maxim Shashkov <Shashkov.ma@icloud.com>
2023-02-19 15:31:21 +0300

    new backgrounds and 1.1 version for appstore

--- Cardholder/Resources/CardStyle.swift
+++ Cardholder/Resources/CardStyle.swift
@@ -10,15 +10,21 @@ import SwiftUI
 enum CardStyle: String, CaseIterable, Codable {
   case blackBG = "Adrien Olichon"
   case blackLeafs = "Elijah O'Donnell"
-  case blueAndYellow = "JL G"
   case space = "Pexels"
   case whitePillars = "cottonbro"
   case colorfulBlur = "David"
+  case pinkSmoke = "rawpixel"
+  case blueLines = "vector_corp"
+  case blackAndRed = "BiZkettE1"
+  case purpleAndPink = "saragnzalez"
+  case goldenLeafs = "rawpixel.com"
   case bluePinkGradient = "shashkov.bluePinkGradient"
   case orangeRedGradient = "shashkov.orangeRedGradient"
   
   var textColor: Color {
     switch self {
+    case .pinkSmoke:
+        return .init(UIColor(red: 50/255, green: 50/255, blue: 50/255, alpha: 1.0))
 //    case .yellowBananas:
 //      return .init(UIColor(hue: 0.1655, saturation: 1, brightness: 1, alpha: 1.0))
     default:
@@ -33,10 +39,17 @@ enum CardStyle: String, CaseIterable, Codable {
       LinearGradient(colors: [.blue, .pink], startPoint: .leading, endPoint: .trailing)
     case .orangeRedGradient:
       LinearGradient(colors: [.orange, .red], startPoint: .top, endPoint: .trailing)
-    case .whitePillars, .colorfulBlur, .blueAndYellow:
+    case .whitePillars,
+            .colorfulBlur,
+            .purpleAndPink,
+            .goldenLeafs:
       Image(self.rawValue)
         .resizable()
         .overlay(.black.opacity(0.2))
+    case .pinkSmoke:
+        Image(self.rawValue)
+            .resizable()
+            .overlay(.white.opacity(0.25))
     default:
       Image(self.rawValue)
         .resizable()
